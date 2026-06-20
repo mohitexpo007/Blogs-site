73336a70c179d5c251ec44a4c677ff81fe81e188
Mohit <arceus93007@gmail.com>
2026-06-20 14:57:51 +0530

    Setting the initial files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Blogs></Blogs>
+      <Pagination></Pagination>
+    </div>  
+  );
+}
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const AppContext=createContext();
+
+function AppContextProvider({children}){
+  const[loading,setloading]=useState(false);
+  const[page,setpage]=useState(1);
+  const[]
+}
